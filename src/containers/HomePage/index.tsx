6ebe194853f7ff438019978400832e55186f860c
ravgusha <ravgusha@gmail.com>
2022-11-05 23:21:23 +0300

    feat: add table component #CC-001

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,7 +1,19 @@
 import Header from '../../components/Header';
+import ButtonLink from '../../components/Link';
+import Table from '../../components/Table';
+
+import './style.scss';
 
 const HomePage = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Table />
+        <ButtonLink label="Добавить организацию" to="/add" />
+      </main>
+    </>
+  );
 };
 
 export default HomePage;
