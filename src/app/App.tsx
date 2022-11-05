aa8c1bf95cadd336d8bf3676085ca8b5bae8017a
ravgusha <ravgusha@gmail.com>
2022-11-05 23:19:46 +0300

    feat: add routes #CC-002

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,18 @@
-
+import { Routes, Route } from 'react-router-dom';
 import HomePage from '../containers/HomePage';
+
+import routes from './routes';
 
 import './style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      {routes.map(({ path, component: Component }, i) => (
+        <Route key={i} path={path} element={<Component />} />
+      ))}
+    </Routes>
   );
 }
 
